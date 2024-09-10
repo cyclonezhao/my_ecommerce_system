9aefaf1ee1538de26d8453bd08382f1f22008da9
zhaoyi <lightseeker@qq.com>
2024-09-10 10:18:18 +0800

    重构：登录接口业务逻辑移到service

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -1,20 +1,10 @@
 package user
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
-	"log"
-	"my_ecommerce_system/pkg/client"
-	"my_ecommerce_system/pkg/config"
-	"my_ecommerce_system/pkg/constant"
 	"my_ecommerce_system/pkg/errorhandler"
-	"my_ecommerce_system/pkg/middleware"
 	"net/http"
-	"time"
-
-	"github.com/golang-jwt/jwt/v4"
-	"golang.org/x/crypto/bcrypt"
 )
 
 func SayHello(writer http.ResponseWriter, request *http.Request) {
@@ -30,45 +20,8 @@ func SignUp(writer http.ResponseWriter, request *http.Request) error{
 		return &errorhandler.BusinessError{Message:"请求无效", HttpCode:http.StatusBadRequest}
 	}
 
-	err := signUp(signInRequest)
+	tokenString, err := signUp(signInRequest)
 
-	if err != nil{
-		// 创建Token
-		tokenString, err := genToken(signInRequest.Username)
-		if err != nil{
-			return err
-		}
-
-		// 把token返回给前端
-		json.NewEncoder(writer).Encode(SignUpRespose{Token: tokenString})
-	}
-	return err
-}
-
-func SignIn(writer http.ResponseWriter, request *http.Request) error {
-	var signInRequest SignUpRequest
-	if err := json.NewDecoder(request.Body).Decode(&signInRequest); err != nil {
-		return &errorhandler.BusinessError{Message:"请求无效", HttpCode:http.StatusBadRequest}
-	}
-
-	// 根据传来的userName从DB中查用户
-	user, err := getUserByName(signInRequest.Username)
-	if user == nil{
-		return &errorhandler.BusinessError{Message:"用户不存在", HttpCode:http.StatusUnauthorized}
-	}else if err != nil{
-		return &errorhandler.BusinessError{Message:err.Error(), HttpCode:http.StatusUnauthorized}
-	}
-
-	userName := user.Name
-	password := user.Password
-
-	// 验证密码
-	if err := bcrypt.CompareHashAndPassword([]byte(password), []byte(signInRequest.Password)); err != nil{
-		return &errorhandler.BusinessError{Message:"用户名或密码错误", HttpCode:http.StatusUnauthorized}
-	}
-
-	// 创建Token
-	tokenString, err := genToken(userName)
 	if err != nil{
 		return err
 	}
@@ -78,31 +31,20 @@ func SignIn(writer http.ResponseWriter, request *http.Request) error {
 	return nil
 }
 
-func genToken(userName string) (string, error){
-	// 创建 JWT Token
-	expirationTime := time.Duration(config.AppConfig.Jwt.Expire) * time.Second
-	claims := &middleware.Claims{
-		UserName: userName,
-		RegisteredClaims: jwt.RegisteredClaims{
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(expirationTime)),
-		},
+func SignIn(writer http.ResponseWriter, request *http.Request) error {
+	var signInRequest SignUpRequest
+	if err := json.NewDecoder(request.Body).Decode(&signInRequest); err != nil {
+		return &errorhandler.BusinessError{Message:"请求无效", HttpCode:http.StatusBadRequest}
 	}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-	// 使用密钥签名token
-	tokenString, err := token.SignedString([]byte(constant.JWT_SECRET_KEY))
+	tokenString, err := signIn(signInRequest)
 	if err != nil{
-		return "", &errorhandler.BusinessError{Message:"创建Token失败"}
+		return err
 	}
 
-	// 将token存到Redis
-	ctx := context.Background()
-	cacheKey := constant.CACHE_USER_TOKEN + ":" + userName
-	err = client.RedisClient.Set(ctx, cacheKey, tokenString, expirationTime).Err()
-	if err != nil {
-		log.Print(err)
-		return "", &errorhandler.BusinessError{Message:"缓存Token失败"}
-	}
-
-	return tokenString, nil
+	// 把token返回给前端
+	json.NewEncoder(writer).Encode(SignUpRespose{Token: tokenString})
+	return nil
 }
+
+
--- internal/user/service.go
+++ internal/user/service.go
@@ -1,13 +1,21 @@
 package user
 
 import (
+	"context"
 	"fmt"
+	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
+	"log"
+	"my_ecommerce_system/pkg/client"
+	"my_ecommerce_system/pkg/config"
+	"my_ecommerce_system/pkg/constant"
 	"my_ecommerce_system/pkg/errorhandler"
-	//"net/http"
+	"my_ecommerce_system/pkg/middleware"
+	"net/http"
+	"time"
 )
 
-func signUp(request SignUpRequest) error{
+func signUp(request SignUpRequest) (string, error) {
 	// 用户名，密码
 	userName := request.Username
 	password := request.Password
@@ -15,9 +23,9 @@ func signUp(request SignUpRequest) error{
 	// 检查用户是否存在
 	exists, err := existsUserName(userName)
 	if err != nil{
-		return err
+		return "", err
 	}else if exists{
-		return &errorhandler.BusinessError{
+		return "", &errorhandler.BusinessError{
 			Message:fmt.Sprintf("用户名[%s]已存在！", userName),
 		}
 	}
@@ -25,7 +33,7 @@ func signUp(request SignUpRequest) error{
 	// 密码哈希化
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil{
-		return fmt.Errorf("密码明文哈希化失败")
+		return "", fmt.Errorf("密码明文哈希化失败")
 	}
 
 	// 创建用户
@@ -34,5 +42,59 @@ func signUp(request SignUpRequest) error{
 		Password: string(hashedPassword),
 	}
 	addNewUser(newUser)
-	return nil
+
+	// 创建Token
+	tokenString, err := genToken(userName)
+	return tokenString, err
+}
+
+func signIn(signInRequest SignUpRequest) (string, error){
+	// 根据传来的userName从DB中查用户
+	user, err := getUserByName(signInRequest.Username)
+	if user == nil{
+		return "", &errorhandler.BusinessError{Message:"用户不存在", HttpCode:http.StatusUnauthorized}
+	}else if err != nil{
+		return "", &errorhandler.BusinessError{Message:err.Error(), HttpCode:http.StatusUnauthorized}
+	}
+
+	userName := user.Name
+	password := user.Password
+
+	// 验证密码
+	if err := bcrypt.CompareHashAndPassword([]byte(password), []byte(signInRequest.Password)); err != nil{
+		return "", &errorhandler.BusinessError{Message:"用户名或密码错误", HttpCode:http.StatusUnauthorized}
+	}
+
+	// 创建Token
+	tokenString, err := genToken(userName)
+	return tokenString, err
+}
+
+func genToken(userName string) (string, error){
+	// 创建 JWT Token
+	expirationTime := time.Duration(config.AppConfig.Jwt.Expire) * time.Second
+	claims := &middleware.Claims{
+		UserName: userName,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(expirationTime)),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// 使用密钥签名token
+	tokenString, err := token.SignedString([]byte(constant.JWT_SECRET_KEY))
+	if err != nil{
+		return "", &errorhandler.BusinessError{Message:"创建Token失败"}
+	}
+
+	// 将token存到Redis
+	ctx := context.Background()
+	cacheKey := constant.CACHE_USER_TOKEN + ":" + userName
+	err = client.RedisClient.Set(ctx, cacheKey, tokenString, expirationTime).Err()
+	if err != nil {
+		log.Print(err)
+		return "", &errorhandler.BusinessError{Message:"缓存Token失败"}
+	}
+
+	return tokenString, nil
 }
