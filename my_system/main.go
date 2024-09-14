b346beb2131e5735b8712d10ae2b1194ccb580fb
zhaoyi <lightseeker@qq.com>
2024-09-15 00:00:26 +0800

    用cmux实现在同一个端口同时处理gRPC和http。
    注意cmux必须调用Serve() 启动，关闭时要调用 close()，不然gRPC服务优雅关闭时会阻塞。

--- my_system/main.go
+++ my_system/main.go
@@ -5,21 +5,25 @@ import (
 	"log"
 	"my_system/grpc/helloworld"
 	"net"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"flag"
 	microservice "my_ecommerce_system/microservice"
 	pb "my_ecommerce_system/my_system_api/grpc/proto/helloworld"
 	my_client "my_ecommerce_system/pkg/client"
 
+	"github.com/gin-gonic/gin"
+	"github.com/soheilhy/cmux"
 	"google.golang.org/grpc"
 	"gopkg.in/yaml.v3"
 )
 
 var (
-	grpcPort = flag.Int("port", 58090, "The server port")
+	grpcPort = flag.Int("port", 8081, "The server port")
 )
 
 type Config struct {
@@ -66,10 +70,18 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+
+	// 创建 cmux 实例，用于在同一个端口同时监听处理gRPC和http请求
+	m := cmux.New(lis)
+	// 匹配 gRPC 请求
+	grpcL := m.MatchWithWriters(cmux.HTTP2MatchHeaderFieldSendSettings("content-type", "application/grpc"))
+	// 匹配 HTTP 请求
+	httpL := m.Match(cmux.HTTP1Fast())
+
 	// 初始化gRPC服务
-	s := grpc.NewServer()
+	grpcServer := grpc.NewServer()
 	// 将gRPC服务和自定义的业务逻辑注册到Greeter服务中
-	pb.RegisterGreeterServer(s, &helloworld.Server{GRPCPort: grpcPort})
+	pb.RegisterGreeterServer(grpcServer, &helloworld.Server{GRPCPort: grpcPort})
 	log.Printf("serving gRPC on %v", lis.Addr())
 
 	// 初始化ETCD注册器
@@ -89,26 +101,55 @@ func main() {
 		log.Fatalf("failed to reg etcd: %v", err)
 	}
 
-	// 启动RPC并监听。另起一个协程运行是为了避免  s.Serve(lis) 阻塞主协程
+	// 启动RPC并监听。另起一个协程运行是为了避免  s.Serve(grpcL) 阻塞主协程
 	// 后续主协程还要监听关闭信号
-	log.Printf("server listening at %v", lis.Addr())
+	log.Printf("server listening at %v", grpcL.Addr())
 	go func() {
-		if err := s.Serve(lis); err != nil {
+		if err := grpcServer.Serve(grpcL); err != nil {
 			log.Fatalf("failed to serve: %v", err)
 			namingService.DelAllEndpoint()
 		}
 	}()
 
+	// 启动HTTP服务
+	engine := gin.Default()
+	engine.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"message": "pong"})
+	})
+
+	httpServer := &http.Server{
+		Handler:        engine,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	go func() {
+		log.Printf("HTTP server listening at %v", grpcL.Addr())
+		if err := httpServer.Serve(httpL); err != nil {
+			log.Fatalf("Failed to serve HTTP: %v", err)
+		}
+	}()
+
+	go func() {
+		if err := m.Serve(); err != nil {
+			log.Fatalf("Failed to serve: %v", err)
+		}
+	}()
+
 	// 等待关闭信号
-	quit := make(chan os.Signal)
+	quit := make(chan os.Signal, 1)
 	// syscall.SIGINT：通常是通过用户在终端按下 Ctrl+C 触发的中断信号
 	// syscall.SIGTERM：是操作系统发送给程序的终止信号，可以通过 kill <pid> 命令发送 SIGTERM 信号
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
-	fmt.Printf("Shutdown Server ... \r\n")
+	fmt.Println("Shutdown Server ...")
+	// cmux必须关掉，不然下面的grpcServer.GracefulStop()会阻塞
+	// 原因猜测是cmux维持着对底层连接的控制，导致 grpcServer.GracefulStop() 持续等待，阻塞停止流程
+	m.Close()
 	// 停止grpc服务
-	s.GracefulStop()
+	grpcServer.GracefulStop()
+	fmt.Println("gRPC服务已停止")
 	// 删除etcd注册信息
 	namingService.DelAllEndpoint()
-	fmt.Printf("Graceful Shutdown Server success\r\n")
+	fmt.Println("Graceful Shutdown Server success.")
 }
